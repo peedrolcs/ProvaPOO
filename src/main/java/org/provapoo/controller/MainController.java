954b0b2577524fea5542827bad65b8b3a247b525
Peedrolcs <pelucasrramos@gmail.com>
2025-11-30 20:22:46 -0300

    Voltando a etapa anterior com o codigo funcional

--- src/main/java/org/provapoo/controller/MainController.java
+++ src/main/java/org/provapoo/controller/MainController.java
@@ -1,14 +1,4 @@
 package org.provapoo.controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+public class MainController {
 }
